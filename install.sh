184ea9b0e0883c54077f5b6d44bab5c959dd7de5
Jakub Stefanski <js@jakubstefanski.com>
2020-01-19 12:29:27 +0100

    Improve installation script

--- install.sh
+++ install.sh
@@ -2,24 +2,42 @@
 
 set -eu
 
-sudo=''
-if ((EUID != 0)); then
-  sudo='sudo'
+case $EUID in
+0) ;;
+*) exec sudo "${0}" "$@" ;;
+esac
+
+if [ -f /etc/os-release ]; then
+	source /etc/os-release
+elif [ -f /usr/lib/os-release ]; then
+	source /usr/lib/os-release
+else
+	echo 'os-release file not found' 1>&2
+	exit 1
 fi
 
-function copy_system_file() {
-  local src=$1
-  local dest=$2
+if [[ "${NAME}" != 'Ubuntu' ]] || [[ "${VERSION_ID}" != '18.04' ]]; then
+	echo "${0} is tested only with Ubuntu 18.04" 1>&2
+	read -r -p "Are you sure you want to continue? [y/N] " answer </dev/tty
+	case ${answer:0:1} in
+	y | Y | yes | YES) ;;
+	*) exit 1 ;;
+	esac
+fi
 
-  ${sudo} mkdir -p "$(dirname "${dest}")"
-  ${sudo} cp "${src}" "${dest}"
-  echo "Installed ${dest}"
+function install_file() {
+	local src=$1
+	local dest=$2
+
+	mkdir -p "$(dirname "${dest}")"
+	cp "${src}" "${dest}"
+	echo "Installed ${dest}"
 }
 
-copy_system_file ./amdgpu-fancontrol /usr/local/bin/amdgpu-fancontrol
-copy_system_file ./amdgpu-fancontrol.service /usr/lib/systemd/system/amdgpu-fancontrol.service
-copy_system_file ./etc-amdgpu-fancontrol.cfg /etc/amdgpu-fancontrol.cfg
+install_file ./amdgpu-fancontrol /usr/local/bin/amdgpu-fancontrol
+install_file ./amdgpu-fancontrol.service /usr/lib/systemd/system/amdgpu-fancontrol.service
+install_file ./etc-amdgpu-fancontrol.cfg /etc/amdgpu-fancontrol.cfg
 
-${sudo} systemctl enable amdgpu-fancontrol.service
-${sudo} systemctl start amdgpu-fancontrol.service
+systemctl enable amdgpu-fancontrol.service
+systemctl restart amdgpu-fancontrol.service
 systemctl status amdgpu-fancontrol.service
